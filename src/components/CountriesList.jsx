cf50a67b98727e20eaaaa358a775f3fd8725e84d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-01 17:18:48 -0400

    Load the countries by continent using actions

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,21 +1,28 @@
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getCountriesByContinent } from '../redux/countries/countriesSlice';
 import SectionTitle from './SectionTitle';
 
-const CountriesList = ({ countriesByContinent }) => (
-  <section className="countries">
-    <SectionTitle title="Stats by countries" />
-    <div className="countries-list">
-      { countriesByContinent }
-    </div>
-  </section>
-);
+const CountriesList = () => {
+  const dispatch = useDispatch();
+  const { selectedContinent } = useSelector((state) => state.continents);
+  const { countriesByContinent } = useSelector((state) => state.countries);
 
-CountriesList.defaultProps = {
-  countriesByContinent: '',
-};
+  useEffect(() => {
+    if(selectedContinent.name) {
+      console.log(selectedContinent.name);
+      dispatch(getCountriesByContinent(selectedContinent.name));
+    }
+  }, [selectedContinent]);
 
-CountriesList.propTypes = {
-  countriesByContinent: PropTypes.string,
+  return (
+    <section className="countries">
+      <SectionTitle title="Stats by countries" />
+      <div className="countries-list">
+        { JSON.stringify(countriesByContinent) }
+      </div>
+    </section>
+  );
 };
 
 export default CountriesList;
