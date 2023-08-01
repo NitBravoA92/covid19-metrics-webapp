cc3d92567402fa32729fa46e448bd3190a9adbfc
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-31 22:53:40 -0400

    Add Header component in Countries page

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
-import Header from '../components/Header';
 import { BsChevronLeft } from 'react-icons/bs';
+import Header from '../components/Header';
 
 const Countries = () => {
   const { countryName } = useParams();
