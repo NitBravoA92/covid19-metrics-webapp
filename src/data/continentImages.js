c0401ca725fcb7db52831cb13127052fc593b0df
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-01 01:42:12 -0400

    Create the continentImages object with the data of components images

---
+++ src/data/continentImages.js
@@ -0,0 +1,17 @@
+import Africa from '../assets/images/Africa.png';
+import Asia from '../assets/images/Asia.png';
+import AustraliaOceania from '../assets/images/AustraliaOceania.png';
+import Europe from '../assets/images/Europe.png';
+import NorthAmerica from '../assets/images/NorthAmerica.png';
+import SouthAmerica from '../assets/images/SouthAmerica.webp';
+
+const continentImages = {
+  'Africa': Africa,
+  'Asia': Asia,
+  'Australia-Oceania': AustraliaOceania,
+  'Europe': Europe,
+  'North America': NorthAmerica,
+  'South America': SouthAmerica,
+};
+
+export default continentImages;
