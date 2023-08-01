8be4aee685b4eeb3c690543cbdb581f845be500f
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-31 22:59:08 -0400

    Change page title of the Home page and the whole app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from '../components/Header';
 
 const Home = () => (
   <>
-    <Header navigation={{ name: 'Covid19', url: '/', page: 'Continents stats' }} />
+    <Header navigation={{ name: 'Covid19', url: '/', page: 'Global disease stats' }} />
     <main>
       List of countries by continents
     </main>
