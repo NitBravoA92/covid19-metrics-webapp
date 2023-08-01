e14f07f2c9c91d760e8f0bcdab2cb67a673e9daa
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-31 23:10:00 -0400

    Update the Header of the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from '../components/Header';
 
 const Home = () => (
   <>
-    <Header navigation={{ name: 'Covid19', url: '/', page: 'Global disease stats' }} />
+    <Header navigation={{ name: <span>Covid19</span>, url: '/', page: 'Global disease stats' }} />
     <main>
       List of countries by continents
     </main>
