c6846bffbde6c9746c0d46a7fac9e84c5eaed4fd
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-01 17:17:58 -0400

    Create the getAllCountries and getCountriesByContinent actions in countriesSlice

--- src/redux/countries/countriesSlice.js
+++ src/redux/countries/countriesSlice.js
@@ -5,17 +5,21 @@ const apiEndpoint = 'https://disease.sh/v3/covid-19/countries';
 
 const actionName = 'countries/getAllCountries';
 
-export const getAllCountries = createAsyncThunk(actionName, async (countriesList, thunkAPI) => {
-  try {
-    const response = await axios.get(`${apiEndpoint}/${countriesList}`);
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
-});
+export const getAllCountries = createAsyncThunk(
+  actionName,
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get(apiEndpoint);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
 
 const initialState = {
   countries: [],
+  countriesByContinent: [],
   isLoading: false,
   error: null,
 };
@@ -23,41 +27,44 @@ const initialState = {
 export const countriesSlice = createSlice({
   name: 'countries',
   initialState,
-  reducers: {},
+  reducers: {
+    getCountriesByContinent: (state, action) => {
+      const continent = action.payload;
+      state.countriesByContinent = state.countries.filter(
+        (country) => country.continent === continent,
+      );
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getAllCountries.pending, (state) => {
-        if (state.countries.length === 0) state.isLoading = true;
+        state.isLoading = true;
       })
       .addCase(getAllCountries.fulfilled, (state, action) => {
-        if (state.countries.length === 0) {
-          state.isLoading = false;
-          state.countries = action.payload.map((country, index) => (
-            {
-              id: (index + 1),
-              name: country.country,
-              flag: country.countryInfo.flag,
-              population: country.population,
-              cases: country.cases,
-              todayCases: country.todayCases,
-              deaths: country.deaths,
-              todayDeaths: country.todayDeaths,
-              recovered: country.recovered,
-              todayRecovered: country.todayRecovered,
-              active: country.active,
-              critical: country.critical,
-              tests: country.tests,
-            }
-          ));
-        }
+        state.isLoading = false;
+        state.countries = action.payload.map((country, index) => ({
+          id: index + 1,
+          continent: country.continent,
+          name: country.country,
+          flag: country.countryInfo.flag,
+          population: country.population,
+          cases: country.cases,
+          todayCases: country.todayCases,
+          deaths: country.deaths,
+          todayDeaths: country.todayDeaths,
+          recovered: country.recovered,
+          todayRecovered: country.todayRecovered,
+          active: country.active,
+          critical: country.critical,
+          tests: country.tests,
+        }));
       })
       .addCase(getAllCountries.rejected, (state, action) => {
-        if (state.countries.length === 0) {
-          state.isLoading = false;
-          state.error = action.payload;
-        }
+        state.isLoading = false;
+        state.error = action.payload;
       });
   },
 });
 
+export const { getCountriesByContinent } = countriesSlice.actions;
 export default countriesSlice.reducer;
