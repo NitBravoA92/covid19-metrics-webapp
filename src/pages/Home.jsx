8fd5dcfc09bec23d1593b0ac593d6578a328f83d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-31 20:28:44 -0400

    Create the Home view component to render the list of Countries by Continent

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const Home = () => (
+  <>
+    <Header navigation={{ name: 'Covid19', url: '/', page: 'Continents stats' }} />
+    <main>
+      List of countries by continents
+    </main>
+  </>
+);
+
+export default Home;
