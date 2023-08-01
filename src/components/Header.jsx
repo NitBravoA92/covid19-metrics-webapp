e130bc4215ee88f4d7bb09e1c3e274471c1f4164
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-31 21:34:00 -0400

    Add the CSS styles of the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { HiOutlineCog, HiOutlineMicrophone } from 'react-icons/hi';
+import '../assets/css/header.css';
 
 const Header = ({ navigation }) => (
   <header>
     <nav>
       <ul id="menu">
-        <li>
+        <li className="pageLink">
           <NavLink to={navigation.url}>{ navigation.name }</NavLink>
         </li>
-        <li>{ navigation.page }</li>
+        <li className="pageName">{ navigation.page }</li>
         <li className="menu-icons">
           <button type="button" aria-label="Microphone"><HiOutlineMicrophone /></button>
           <button type="button" aria-label="Cog"><HiOutlineCog /></button>
