f3e1fcacd4ef8eca55977fee65477e9b316198a6
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-02 18:58:59 -0400

    Create the list of countries by continent using the CountryItem component

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,12 +1,27 @@
+import { useSelector } from 'react-redux';
 import SectionTitle from './SectionTitle';
+import CountryItem from './CountryItem';
 
-const CountriesList = () => (
-  <section className="countries">
-    <SectionTitle title="Stats by countries" />
-    <div className="countries-list">
-      hello
-    </div>
-  </section>
-);
+const CountriesList = () => {
+  const { countries } = useSelector((state) => state.countries);
+
+  return (
+    <section className="countries">
+      <SectionTitle title="Stats by countries" />
+      <div className="countries-list">
+        { countries.map(({
+          id, flag, name, cases,
+        }) => (
+          <CountryItem
+            key={id}
+            image={flag}
+            name={name}
+            cases={cases}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default CountriesList;
