b9cab47a5e532d2fe98a04abdbdfb68149152d3a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-02 18:59:35 -0400

    Render the List of countries by continent and the Continent general data

--- src/pages/CountriesDetails.jsx
+++ src/pages/CountriesDetails.jsx
@@ -1,9 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { BsChevronLeft } from 'react-icons/bs';
 import { getAllCountries } from '../redux/countries/countriesSlice';
+import { getContinent } from '../redux/continents/continentsSlice';
+import continentImages from '../data/continentImages';
 import Header from '../components/Header';
+import Headline from '../components/Headline';
+import CountriesList from '../components/CountriesList';
+import '../assets/css/countries.css';
 
 const CountriesDetails = () => {
   const { continentName } = useParams();
@@ -11,21 +16,33 @@ const CountriesDetails = () => {
     ? continentName
     : continentName.replace(/-/g, ' ');
 
-  const dispatch = useDispatch();
+  const { selectedContinent } = useSelector((state) => state.continents);
 
-  const { countries } = useSelector((state) => state.countries);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCountries(realContinentName));
-  }, [dispatch]);
+    dispatch(getContinent(realContinentName));
+  }, [dispatch, realContinentName]);
 
   return (
     <>
       <Header
-        navigation={{ name: <BsChevronLeft />, url: '/', page: 'Countries stats' }}
+        navigation={{ name: <BsChevronLeft />, url: '/', page: 'Continent stats' }}
       />
       <main>
-        { JSON.stringify(countries) }
+        <Headline
+          srcImg={continentImages[realContinentName]}
+          name={realContinentName}
+          information={[
+            { stats: selectedContinent?.population, text: 'inhabitants' },
+            { stats: selectedContinent?.cases, text: 'total cases' },
+            { stats: selectedContinent?.deaths, text: 'deaths' },
+            { stats: selectedContinent?.recovered, text: 'recovered' },
+            { stats: selectedContinent?.active, text: 'active cases' },
+          ]}
+        />
+        <CountriesList />
       </main>
     </>
   );
