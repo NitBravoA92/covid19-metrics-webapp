f49d381fcf2c6cb8f0b41ad5b1ce77e5e39aa628
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-02 18:54:11 -0400

    Refactor the Headline component to add stats dynamically

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,18 +1,24 @@
 import PropTypes from 'prop-types';
 import '../assets/css/headline.css';
 
-const Headline = ({ srcImg, name, totalCases }) => (
+const Headline = ({ srcImg, name, information }) => (
   <section className="headline">
     <div className="region image">
-      <img src={srcImg} alt={name} />
+      <img src={srcImg} alt={name} className="opacity-5" />
     </div>
     <div className="region info">
       <h1 className="name">{ name }</h1>
-      <p className="stats-counter">
-        { totalCases }
-        {' '}
-        cases
-      </p>
+      {
+        information.map(
+          ({ stats, text }) => (
+            <p className="stats-counter" key={text}>
+              { stats }
+              {' '}
+              { text }
+            </p>
+          ),
+        )
+      }
     </div>
   </section>
 );
@@ -20,13 +26,16 @@ const Headline = ({ srcImg, name, totalCases }) => (
 Headline.defaultProps = {
   srcImg: '',
   name: '',
-  totalCases: 0,
+  information: [],
 };
 
 Headline.propTypes = {
   srcImg: PropTypes.string,
   name: PropTypes.string,
-  totalCases: PropTypes.number,
+  information: PropTypes.arrayOf(PropTypes.shape({
+    stats: PropTypes.string,
+    text: PropTypes.string,
+  })),
 };
 
 export default Headline;
