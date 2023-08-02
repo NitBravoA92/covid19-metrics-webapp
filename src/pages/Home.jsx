119528b2fba81a8db982822cf0bb491644a81bf3
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-02 02:38:36 -0400

    Update the Home page ti display the list of continents and the global stats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,31 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { getAllContinents, updateSelectedContinent } from '../redux/continents/continentsSlice';
-import { getAllCountries } from '../redux/countries/countriesSlice';
+import { getAllContinents } from '../redux/continents/continentsSlice';
 import Header from '../components/Header';
 import Headline from '../components/Headline';
-import CountriesList from '../components/CountriesList';
-import continentImages from '../data/continentImages';
+import ContinentsList from '../components/ContinentsList';
+import Continents from '../assets/images/Continents.png';
 import '../assets/css/home.css';
 
 const Home = () => {
+  const { totalCases } = useSelector((state) => state.continents);
+
   const dispatchActions = useDispatch();
-  const { continents, selectedContinent } = useSelector((state) => state.continents);
 
   useEffect(() => {
     dispatchActions(getAllContinents());
-    dispatchActions(getAllCountries());
-  }, []);
-
-  const handleContinentsSelection = (e) => {
-    const continent = e.target.value;
-    dispatchActions(updateSelectedContinent(continent));
-  };
+  }, [dispatchActions]);
 
   return (
     <>
-      <Header navigation={{ name: <span>Covid19</span>, url: '/', page: 'Global disease stats' }} />
+      <Header navigation={{ name: <span>Covid19</span>, url: '/', page: 'Global cases stats' }} />
       <main>
-        <div className="continents-dropdown">
-          <select className="continents-options" defaultValue={selectedContinent.name} onChange={handleContinentsSelection}>
-            {
-              continents.map((continent) => (
-                <option key={continent.id} value={continent.name}>{continent.name}</option>
-              ))
-            }
-          </select>
-        </div>
         <Headline
-          srcImg={continentImages[selectedContinent.name]}
-          name={selectedContinent.name}
-          totalCases={selectedContinent.cases}
+          srcImg={Continents}
+          name="World Statistics"
+          totalCases={totalCases}
         />
-        <CountriesList />
+        <ContinentsList />
       </main>
     </>
   );
