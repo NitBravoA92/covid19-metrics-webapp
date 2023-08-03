156ca1935fd41515ef63aa1ff3902d07bea3eeb5
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-03 10:53:42 -0400

    Display the list of country stats using the CountryStatItem component

--- src/pages/CountryStats.jsx
+++ src/pages/CountryStats.jsx
@@ -3,9 +3,25 @@ import { useParams } from 'react-router-dom';
 import { BsChevronLeft } from 'react-icons/bs';
 import Header from '../components/Header';
 import Headline from '../components/Headline';
+import SectionTitle from '../components/SectionTitle';
+import CountryStatItem from '../components/CountryStatItem';
 
 const CountryStats = () => {
   const { continentName, countryName } = useParams();
+
+  const realCountryName = countryName.replace(/-/g, ' ');
+
+  const { countries } = useSelector((state) => state.countries);
+
+  const selectedCountry = countries.find((country) => country.name === realCountryName);
+
+  const filteredStats = Object.keys(selectedCountry)
+    .filter((item) => (
+      item !== 'id'
+      && item !== 'name'
+      && item !== 'continent'
+      && item !== 'population'
+      && item !== 'flag'));
 
   return (
     <>
@@ -14,12 +30,23 @@ const CountryStats = () => {
       />
       <main>
         <Headline
-          srcImg=""
-          name={countryName}
+          srcImg={selectedCountry?.flag}
+          name={realCountryName}
           information={[
-            { stats: '0', text: 'inhabitants' },
+            { stats: selectedCountry?.population, text: 'inhabitants' },
           ]}
         />
+        <section className="country-stats">
+          <SectionTitle title="Country General Stats" />
+          <ul className="country-details">
+            {
+            filteredStats
+              .map((title) => (
+                <CountryStatItem key={title} title={title} amount={selectedCountry[title]} />
+              ))
+        }
+          </ul>
+        </section>
       </main>
     </>
   );
