40055df281f99480c17ac96c2e82139fc93d12ee
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-03 02:38:22 -0400

    Create the CountryStats view component to display information about each country

--- src/pages/CountryStats.jsx
+++ src/pages/CountryStats.jsx
@@ -1,13 +1,27 @@
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { BsChevronLeft } from 'react-icons/bs';
+import Header from '../components/Header';
+import Headline from '../components/Headline';
 
 const CountryStats = () => {
   const { continentName, countryName } = useParams();
+
   return (
-    <main>
-      { continentName }
-      {' '}
-      { countryName }
-    </main>
+    <>
+      <Header
+        navigation={{ name: <BsChevronLeft />, url: `/${continentName}`, page: 'Country stats' }}
+      />
+      <main>
+        <Headline
+          srcImg=""
+          name={countryName}
+          information={[
+            { stats: '0', text: 'inhabitants' },
+          ]}
+        />
+      </main>
+    </>
   );
 };
 
