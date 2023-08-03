55a248f08fb95f8ae3807487a0783fa602dbc579
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-03 11:25:22 -0400

    Add CSS styles to the Country stats page

--- src/pages/CountryStats.jsx
+++ src/pages/CountryStats.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Headline from '../components/Headline';
 import SectionTitle from '../components/SectionTitle';
 import CountryStatItem from '../components/CountryStatItem';
+import '../assets/css/countryStats.css';
 
 const CountryStats = () => {
   const { continentName, countryName } = useParams();
