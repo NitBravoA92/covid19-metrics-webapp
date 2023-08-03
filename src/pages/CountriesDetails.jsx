3e02d5037243f86ba0bea24ff9c82dda8f358801
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-03 15:27:05 -0400

    Create a search bar to filter the countries by their names

--- src/pages/CountriesDetails.jsx
+++ src/pages/CountriesDetails.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { BsChevronLeft } from 'react-icons/bs';
 import continentImages from '../data/continentImages';
 import Header from '../components/Header';
@@ -11,16 +12,46 @@ import '../assets/css/countries.css';
 const CountriesDetails = () => {
   const { continentName } = useParams();
 
+  const { continents } = useSelector((state) => state.continents);
+  const { countries } = useSelector((state) => state.countries);
+
   const realContinentName = continentName === 'Australia-Oceania'
     ? continentName
     : continentName.replace(/-/g, ' ');
 
-  const { continents } = useSelector((state) => state.continents);
-  const { countries } = useSelector((state) => state.countries);
-
   const selectedContinent = continents.find((continent) => continent.name === realContinentName);
 
-  const filteredCountries = countries.filter((country) => country.continent === realContinentName);
+  const [filteredCountries, setFilteredCountries] = useState({
+    countries: [],
+    searchedCountries: [],
+  });
+
+  const handlerSearch = (e) => {
+    const country = e.target.value;
+    if (country === '') {
+      setFilteredCountries((filteredCountries) => ({
+        ...filteredCountries,
+        searchedCountries: filteredCountries.countries,
+      }));
+    } else {
+      const newFilteredCountries = filteredCountries.countries.filter(
+        (item) => item.name.toUpperCase().includes(country.toUpperCase()),
+      );
+      setFilteredCountries((filteredCountries) => ({
+        ...filteredCountries,
+        searchedCountries: newFilteredCountries,
+      }));
+    }
+  };
+
+  useEffect(() => {
+    const initialCountries = countries.filter((country) => country.continent === realContinentName);
+    setFilteredCountries((filteredCountries) => ({
+      ...filteredCountries,
+      countries: initialCountries,
+      searchedCountries: initialCountries,
+    }));
+  }, [countries]);
 
   return (
     <>
@@ -39,11 +70,18 @@ const CountriesDetails = () => {
             { stats: selectedContinent?.active, text: 'active cases' },
           ]}
         />
-
+        <section className="continent-stats-search">
+          <input
+            type="text"
+            placeholder="Search country"
+            id="country-searcher"
+            onChange={handlerSearch}
+          />
+        </section>
         <section className="countries">
           <SectionTitle title="Stats by countries" />
           <div className="countries-list">
-            {filteredCountries.map(({
+            {filteredCountries.searchedCountries.map(({
               id, flag, name, cases,
             }) => (
               <CountryItem key={id} image={flag} name={name} cases={cases} />
