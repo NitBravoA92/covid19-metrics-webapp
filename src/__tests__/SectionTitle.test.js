043adb0f2ef4c1cdeb2ce8e3a4eecf07e8278077
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-08-04 09:43:09 -0400

    Create the tests cases for the SectionTitle component

---
+++ src/__tests__/SectionTitle.test.js
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import SectionTitle from "../components/SectionTitle";
+
+describe("The SectionTitle component", () => {
+  test("should render correctly into the DOM", () => {
+    const { sectionTitleComponent } = render(
+      <SectionTitle title="STATS BY CONTINENTS" />
+    );
+    expect(sectionTitleComponent).toMatchSnapshot();
+  });
+
+  test("should render with the text: STATS BY COUNTRIES", () => {
+    const longTitle =
+      "STATS BY COUNTRIES";
+    render(<SectionTitle title={longTitle} />);
+    const sectionTitle = screen.getByText(longTitle);
+    expect(sectionTitle).toBeInTheDocument();
+  });
+});
